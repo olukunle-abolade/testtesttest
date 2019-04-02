78df009a8839f03ad35836433f8a4d5bcbbc8660
Olukunle.Abolade <olukunle.abolade@wemabank.com>
2019-04-02 11:34:28 +0100

    added one more value to get values controller

--- Sample1/Controllers/ValuesController.cs
+++ Sample1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Sample1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
